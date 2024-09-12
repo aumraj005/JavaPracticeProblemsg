aa935930c18621a0c3f1e71629b1b21018ba5e58
aumraj005 <128635875+aumraj005@users.noreply.github.com>
2024-09-12 11:24:23 +0530

    Add files via upload

---
+++ SumOfPrimaryDiagnal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumOfPrimaryDiagnal {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner sc= new Scanner(System.in);
+        int n =  sc.nextInt();
+        int m  = sc.nextInt();
+        int [][] arr = new int [n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr [i] [j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(i==j) {
+                    sum += arr[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
